aca21bf99e9e1362301947308e3f74cbc9695203
Rafael Francisco <5050440+rafaelfrancisco-dev@users.noreply.github.com>
2024-08-25 02:22:26 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,11 +5,11 @@ import PackageDescription
 let package = Package(
 	name: "Queue",
 	platforms: [
-		.macOS(.v10_16),
-		.macCatalyst(.v14),
+		.macOS(.v10_15),
+		.macCatalyst(.v13),
 		.iOS(.v14),
-		.tvOS(.v14),
-		.watchOS(.v7)
+		.tvOS(.v13),
+		.watchOS(.v6)
 	],
 	products: [
 		.library(name: "Queue", targets: ["Queue"]),
